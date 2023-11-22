be3f4d1e1bff98b93818e92ab48e70cd31474da8
saahhh <143372306+saahhh@users.noreply.github.com>
2023-11-22 17:07:44 +0900

    spring db

---
+++ spring-DB-Oracle-pre/src/main/java/com/kh/springdb/model/Board.java
@@ -0,0 +1,35 @@
+package com.kh.springdb.model;
+
+import java.sql.Timestamp;
+
+public class Board {
+	private int board_id;
+	private String title;
+	private String content;
+	private String author;
+	
+	public int getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}

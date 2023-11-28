784c6da0f0ef45c1f4df8fe004bf9ba956b33045
saahhh <143372306+saahhh@users.noreply.github.com>
2023-11-28 11:50:54 +0900

    spring jpa

---
+++ spring-JPA-Shop/src/main/java/com/kh/shop/repository/ProductRepository.java
@@ -0,0 +1,45 @@
+package com.kh.shop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.kh.springdb.vo.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	//메서드 이름으로 간단하게 쿼리를 정의하기
+	Product findByProductName(String product_name);
+	
+	//전체제품 출력하는 메서드 추가
+	List<Product> findByPriceDesc(Integer price); //interface에서는 int가 먹히지 않아 Integer로 표기
+	
+	//만약에 repository에 쿼리를 추가하고 싶을 경우
+	@Query("SELECT * FROM ProductItem WHERE price desc")
+	List<Product> findByDetail(@Param("category") String category); //import 시 org.springframework.data.jpa.repository.JpaRepository; 선택
+		
+	
+	/*
+	public void CreateProductList() {
+		Product product = new Product();
+		product.setProduct_name("테스터 제품");
+		product.setPrice(1000);
+		product.setCategory("가전");
+	}
+	*/
+}
+
+
+ /*
+ 	Repository 
+ 		Spring Data JPA에서 제공하는 인터페이스
+ 		데이터베이스에서 User 엔터티에 접근하는데 사용
+ 		기본적인 CRUD 작업을 수행할 수 있는 메서드를 제공한다
+ 		
+ 		예를들어
+ 			조회 : 전체조회(findAll) , 아이디 하나만 조회(findById) 와 같은 메서드
+ 			저장 : save
+ 			삭제 : deleteById(부분삭제)
+ 	
+ */
--- spring-JPA-Shop/src/main/java/com/kh/springdb/vo/Product.java
+++ spring-JPA-Shop/src/main/java/com/kh/springdb/vo/Product.java
@@ -1,23 +1,40 @@
 package com.kh.springdb.vo;
 
+import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name="ProductItem") //테이블을 생성해주는 어노테이션
 public class Product {
-	@Getter
-	@Setter
-	private int product_id;
-	@Getter
-	@Setter
+	@Id
+	@Column(name="product_id")
+	private Long product_id;
+	
+	@Column(nullable = false, length = 50)
 	private String product_name;
-	@Getter
-	@Setter
+	
+	@Column(nullable = false, length = 50)
 	private String category;
-	@Getter
-	@Setter
+	
+	@Column(name="price")
 	private int price;
-	@Getter
-	@Setter
-	private int stock_quantity;
+	
 }
+
+/*
+	@Table : 테이블 이름을 지정해주는 어노테이션
+	@Id : 해당 필드가 엔터티의 primaryKey임을 나타낸다
+	@Column : 해당 필드가 데이터베이스 테이블의 컬럼에 매핑되는 것을 확인할 수 있다
+		nullable : 해당 컬럼이 null값을 허용하는지 여부를 나타낸다. (JPA안에 들어가있는 공식)
+		length : 문자열 컬럼의 최대 길이를 지정하는 것
+		String으로 시작되는 필드값의 경우 String으로 지정된 이름으로 명시되기 때문에 따로 name을 지정해주지 않아도 되지만 String 이외의 값은 name을 설정해주어 column명을 지정해주는 것이 원칙이다
+				
+*/
+ 

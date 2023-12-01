a3ac693127f0b861347585917b3b922028608258
saahhh <143372306+saahhh@users.noreply.github.com>
2023-12-01 17:42:58 +0900

    spring jpa

--- spring-JPA-MyShop/src/main/java/com/kh/spring/shop/controller/ShopController.java
+++ spring-JPA-MyShop/src/main/java/com/kh/spring/shop/controller/ShopController.java
@@ -1,9 +1,14 @@
 package com.kh.spring.shop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.kh.spring.shop.service.ShopService;
+import com.kh.spring.shop.vo.Product;
 
+//주문과 결제
 public class ShopController {
 
 	@Autowired
@@ -12,4 +17,39 @@ public class ShopController {
 	public ShopController(ShopService shopService) {
 		this.shopService = shopService;
 	}
+	
+	//주문에 관련된 내용을 처리하는 메서드
+	@PostMapping("/placeOrder")
+	public String placeOrder(@RequestBody Product product, int quantity, Model model) {
+		//주문을 처리하고 주문한 결과를 Order에 반환하겠다
+		shopService.placeOrder(product, quantity);
+			
+		//모델에 필요한 데이터를 추가해준다
+		model.addAttribute("msg", "주문이 성공적으로 전달되었습니다.");
+		//주문확인 페이지로 이동
+		return "redirect:/orderCheck";
+		
+	}
+	
+	@PostMapping("/paymentFinish")
+	public String processPayment(Long orderId, String paymentStatus, Model model) {
+		//주문에 대한 결제를 처리하고 payment를 반환
+		shopService.savePayment(orderId, paymentStatus);
+		model.addAttribute("msg", "결제가 성공적으로 처리되었습니다.");
+		return "redirect:/paymentCheck";
+		
+	}
+	
+		
 }
+
+
+/*
+	@RequestBody
+		정보를 URL이 아니라 자바 객체로 받는다는 것
+		요청했던 body에 위치하도록 할 때 사용한다 (html에서 body부분)
+	@RequestParam
+		정보를 URL에 저장한다는 것 (html에서 url부분)
+	
+ */
+ 
--- spring-JPA-MyShop/src/main/java/com/kh/spring/shop/service/ShopService.java
+++ spring-JPA-MyShop/src/main/java/com/kh/spring/shop/service/ShopService.java
@@ -1,15 +1,20 @@
 package com.kh.spring.shop.service;
 
+import com.kh.spring.shop.repository.OrderRepository;
+import com.kh.spring.shop.repository.PaymentRepository;
 import com.kh.spring.shop.repository.ProductRepository;
 import com.kh.spring.shop.vo.Cart;
 import com.kh.spring.shop.vo.Order;
+import com.kh.spring.shop.vo.Payment;
 import com.kh.spring.shop.vo.Product;
 
 public class ShopService {
 	//최종적으로 만들어준 order 와 cart 정보를 가지고 결제를 하거나 장바구니에 상품을 추가해서 주문을 생성해주는 메서드들을 넣어줄 것
 	public Cart cart = new Cart();	
 	
-	public ProductRepository productRepository;
+	private ProductRepository productRepository; //sql문은 보안이 중요하기 때문에 private으로 선언해주는 것이 좋다
+	private PaymentRepository paymentRepository;
+	private OrderRepository orderRepository;
 	
 	public Order placeOrder(Product product, int quantity) {
 		//장바구니에 상품 추가
@@ -33,19 +38,28 @@ public class ShopService {
 	
 	//주문정보 조회 메서드
 	public Order getOrderById(Long orderId) {
-		Order order = new Order();
+		/*Order order = new Order();
 		order.setId(orderId);
 		order.setProduct(getProductById(orderId));
 		//주문한 수량 추가
-		return order;
+		return order; 부분 대신 아래 구문으로!!*/
+		return orderRepository.findById(orderId).orElse(null);
 	}
 	
 	//상품정보 조회 메서드
 	public Product getProductById(Long productId) {
-		return productRepository.getProductById(productId);
-		
+		return productRepository.findById(productId).orElse(null);	
+		//.orElse(null) : Optional 대신 쓰는 방법
 	}
 	
 	//결제처리 메서드
+	private Payment savePayment(Order order, String payment) {
+		//결제에 대한 정보 처리
+		Payment payments = new Payment();
+		payments.setOrder(order);
+		payments.setPaymentStatus(payment);
+		return payments;
+		
+	}
 	
 }
